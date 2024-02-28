6eb995c0c9bae5ddd0c1856554b441652784a8c1
Gleison <gleisonrgomes@gmail.com>
2024-02-27 21:09:20 -0300

    sistema actor feito com sucesso!

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { testConnection } from "./database/connection.js";
+import { routes } from "./routes/index.route.js";
+
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+app.use(routes)
+
+app.listen(port, () => {
+    testConnection();
+    console.log(`Servidor rodando na porta ${port}`);
+});
---
+++ src/shared/messages.js
@@ -0,0 +1,11 @@
+const ERRORS = {
+    ALREADY_EXISTS: "já existe, verifique os dados e tente novamente!",
+    NOT_FOUND: "não encontrado(a), verifique os dados e tente novamente!"
+}
+
+const SUCESS = {
+    CREATED: "criado(a) com sucesso!",
+    DELETED: "deletado(a) com sucesso!"
+}
+
+export { ERRORS, SUCESS }
