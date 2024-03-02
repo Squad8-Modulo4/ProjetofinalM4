0122637acd7d3b9cc8d94d0b92f1bb92002b85e6
Ingrid <oligrid20@gmail.com>
2024-03-01 21:29:17 -0300

    CRUDgenre

--- src/controllers/genre.controller.js
+++ src/controllers/genre.controller.js
@@ -1,6 +1,6 @@
 
 import {GenreEntity} from "../entities/Genre.entity.js";
-import { SUCESS } from "../shared/messages.js";
+import {ERRORS, SUCESS} from "../shared/messages.js";
 const createGenre = async (rec,res)=>{
     try{
     await GenreEntity.sync()
@@ -8,20 +8,59 @@ const createGenre = async (rec,res)=>{
     const createGenre = await GenreEntity.create({
             name
     });
-    res.status(201).json({message:`Genênero ${SUCESS.CREATED}`, createGenre, });
+    return  res.status(201).json({message:`Genênero ${SUCESS.CREATED}`, createGenre, });
     }
     catch(error){
-    res.status(500).json({message:error})
+    return  res.status(500).json({message:error})
     }
 }
 const getAllGeners = async(req,res)=> {
     try {
         const geners= await GenreEntity.findAll()
-        res.json({geners});
+        return  res.json({geners});
 
     } catch (error){
-        res.status(500).json({ message: error });
+        return  res.status(500).json({ message: error });
     }
 }
-export {createGenre,getAllGeners}
+
+const updateGenre = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const {name} = req.body;
+        const genreId =   await  GenreEntity.findByPk();
+        if(!genreId){
+            res.json({ message: `Genre ${ERRORS.NOT_FOUND}` });
+            await  GenreEntity.update( {name} ,{
+                where: {
+                    id
+                }
+            });
+
+
+        }
+    }catch (error){
+       return  res.status(500).json({ message: error });
+    }
+}
+const deleteGenre = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const {name} = req.body;
+        const genreId =   await  GenreEntity.findByPk();
+        if(!genreId){
+            res.json({ message: `Genre ${ERRORS.NOT_FOUND}` });
+            await  GenreEntity.destroy( {name:name} ,{
+                where: {
+                    id
+                }
+            });
+
+
+        }
+    }catch (error){
+        return  res.status(500).json({ message: error });
+    }
+}
+export {createGenre,getAllGeners, updateGenre,deleteGenre}
 
--- src/routes/genre.route.js
+++ src/routes/genre.route.js
@@ -1,9 +1,13 @@
 
 import { Router } from "express" ;
-import {createGenre,getAllGeners} from "../controllers/genre.controller.js";
+import {createGenre, getAllGeners,updateGenre,deleteGenre } from "../controllers/genre.controller.js";
 
 const  genreRouter = Router()
 
-genreRouter.post("/new-genre", createGenre)
-genreRouter.get("/show-genres", getAllGeners)
+genreRouter.post("/new-genre", createGenre);
+genreRouter.get("/show-genres", getAllGeners);
+genreRouter.patch("/name-update/:id", updateGenre);
+genreRouter.delete("/delete-genre/:id",deleteGenre);
+
+
 export {genreRouter}
