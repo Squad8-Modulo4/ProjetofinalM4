b1c17ea1dc98c18e393d1bc700eebb655ca2d840
Gleison <gleisonrgomes@gmail.com>
2024-03-06 17:36:06 -0300

    inserindo banco de dados sqlite

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,12 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_DATABASE,process.env.DB_USERNAME,process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
-    logging: false
-});
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'path/to/database.sqlite'
+  });
 
 const testConnection = async () => {
     try{
