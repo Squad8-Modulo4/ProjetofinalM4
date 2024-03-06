ca4cbd2338fd81397ff39e7610abf427fd24f34b
Gleison <gleisonrgomes@gmail.com>
2024-03-06 15:10:04 -0300

    merge com a teste

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { movieRoute } from "./movie.route.js";
 import { actorRoute } from "./actor.route.js";
-import { directorRoute } from "./director.routes.js";import { genreRouter } from "./genre.route.js";
+import { directorRoute } from "./director.routes.js";
+import { genreRouter } from "./genre.route.js";
 
 const routes = Router();
 
 routes.use(actorRoute)
 routes.use(movieRoute)
 routes.use(genreRouter)
-
 routes.use(directorRoute);
+
 export { routes }
