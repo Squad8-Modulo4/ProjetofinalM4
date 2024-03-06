0941c0d007a1e9ea72519d6dfc08de504c50f733
Gleison <gleisonrgomes@gmail.com>
2024-03-06 14:58:32 -0300

    adicionando sistema de Director

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { actorRoute } from "./actor.route.js";
-
+import { directorRoute } from "./director.routes.js";
 const routes = Router();
 
 routes.use(actorRoute);
-
+routes.use(directorRoute);
 export { routes }
